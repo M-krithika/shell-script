ceb35b5b4747a391dea8d8fbd89d6e510510703d
krithika <krithika@gmail.com>
2024-04-16 18:55:14 +0530

    new file

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "Ramesh:: Hello suresh. how are you?"
-echo "suresh:: Hii ramesh. Iam fine...How are you?"
-echo "Ramesh:: I am fine too. How is your work?"
-echo "suresh:: not bad. I am thunking to upgrade to devops."
+echo "$PERSON1:: Hello $PERSON2h. how are you?"
+echo "$PERSON2:: Hii $PERSON1. Iam fine...How are you?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2:: not bad. I am thunking to upgrade to devops."
