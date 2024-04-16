85c4830ed442ada7627444d7242807749f25978e
krithika <krithika@gmail.com>
2024-04-16 19:08:03 +0530

    new file

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -3,5 +3,5 @@
 MOVIES=("okkadu" "athadu" "murari")
 
 echo "first movie is : ${MOVIES[0]}"
-echo "second movie is : ${MOVIES[0]}"
+echo "second movie is : ${MOVIES[1]}"
 echo "All movies are : ${MOVIES[@]}"
