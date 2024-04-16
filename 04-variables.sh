47f8b6519c196dffa780f31f240aa5ebc130a18d
krithika <krithika@gmail.com>
2024-04-16 18:51:19 +0530

    new file

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "Ramesh:: Hello suresh. how are you?"
+echo "suresh:: Hii ramesh. Iam fine...How are you?"
+echo "Ramesh:: I am fine too. How is your work?"
+echo "suresh:: not bad. I am thunking to upgrade to devops."
