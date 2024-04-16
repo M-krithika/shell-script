41644d8652cf9b599ce457733e85018ee031e64d
krithika <krithika@gmail.com>
2024-04-16 18:31:52 +0530

    file is variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+$PERSON1=Ramesh $PERSON2=suresh
+
+echo "$PERSON1:: Hello $PERSON2h. how are you?"
+echo "$PERSON2:: Hii $PERSON1. Iam fine...How are you?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2:: not bad. I am thunking to upgrade to devops."
