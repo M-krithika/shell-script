671a18e375558669fa6ff4e29a693c0dc3f15900
krithika <krithika@gmail.com>
2024-04-16 18:23:52 +0530

    file is updated

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-This is again sample script
+#!/bin/bash
+
+echo "Ramesh:: Hello suresh. how are you?"
+echo "suresh:: Hii ramesh. Iam fine...How are you?"
+echo "Ramesh:: I am fine too. How is your work?"
+echo "suresh:: not bad. I am thunking to upgrade to devops."
