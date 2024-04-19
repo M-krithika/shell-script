7b412f6f28958d2785895881e72fa52f3b582428
krithika <krithika@gmail.com>
2024-04-19 10:51:20 +0530

    functions in shell script

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
 VALIDATE(){
     echo "exit status: $1"
     echo "what are you doing: $2"
 }
 
-if [USERID -ne 0]
+if [ $USERID -ne 0]
 then
     echo "please run this script with root access."
     exit 1
