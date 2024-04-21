57a237add05caf49e972894a27fa81a2e83bf128
krithika <krithika@gmail.com>
2024-04-21 21:14:43 +0530

    colors in shell script

---
+++ 11-colors.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[33m"
+Y="\e[33m"
+B="\e[34m"
+P="\e[35m"
+N="\e[0m"
+
+echo "script started executing at: $TIMESTAMP"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e "$2...$R FAILURE $N"
+    exit 1
+
+else
+    echo -e "$2...$G SUCCESS $N"
+
+fi
+}
