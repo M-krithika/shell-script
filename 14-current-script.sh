d8d91a9a772a9179b708e6ea9364a61603d2965b
M-krithika <krithikanani8@gmail.com>
2024-04-25 20:02:15 +0530

    updated shell script

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -4,7 +4,7 @@ COURSE= "Devops for current script"
 echo "Before calling other script, course= $COURSE"
 echo "PID of current script: $$"
 
-#./15-other-script.sh
+./15-other-script.sh
 
 echo "after calling other script, course= $COURSE"
 echo "PID of current script: $$"
