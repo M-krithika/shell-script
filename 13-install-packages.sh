6d22eb0ab34715b0dcd01611d2a7ba2c0bf76e6b
M-krithika <krithikanani8@gmail.com>
2024-04-25 20:29:17 +0530

    updated shell script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -32,16 +32,8 @@ else
     echo "you are super user"
 fi
 
-for i in $@
-do
-  echo "package to install: $i"
-  dnf list installed $1&>>$LOGFILE
-  if [ $? -ee 0 ]
-  then
-    echo -e "si already installed...$Y SKIPPING $N"
-  else
-    dnf install $i -y &>>$LOGFILE
-  VALIDATE $? "Installation of $i"
-fi
-done
+dnf install mysqlll -y
+
+dnf install git -y
+  
 
